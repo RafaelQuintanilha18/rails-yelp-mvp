4aca97f2b003058731280d35c7a421f0773ab6e5
Rafael Quintanilha <rafael.quintanilha18@gmail.com>
2022-08-17 15:47:17 +0100

    Completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,10 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+  # movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Restaurant.create(name: 'YokYou', address: 'Porto', phone_number: '51651-515', category: 'chinese')
+Restaurant.create(name: 'Italian Cafe', address: 'Roma', phone_number: '515651', category: 'italian')
+Restaurant.create(name: 'Sushi Awesome', address: 'Rio de Janeiro', phone_number: '51622-652', category: 'japonese')
+Restaurant.create(name: 'French Restaurant', address: 'Lyon', phone_number: '51651-5151', category: 'french')
+Restaurant.create(name: 'Belgian Restaurant', address: 'Porto', phone_number: '51651-515', category: 'belgian')
