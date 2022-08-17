b8fcb6e1806ec4aee481c84db5ae3400d49ed27f
Rafael Quintanilha <rafael.quintanilha18@gmail.com>
2022-08-17 17:00:08 +0100

    Completed with links

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-  get 'reviews/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :restaurants
-
   resources :restaurants do
     resources :reviews, only: [:new, :create]
   end
